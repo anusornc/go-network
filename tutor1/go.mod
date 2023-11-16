d7cdff1349c40a7646eb81d5978fd272983ffc6c
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2023-11-16 14:57:15 +0700

    Add new Hello function

---
+++ tutor1/go.mod
@@ -0,0 +1,3 @@
+module github.com/anusornc/go-network/tutor1
+
+go 1.20
---
+++ tutor1/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+// main is the entry point for the application.
+func main() {
+	fmt.Println("Hello, World!")
+}
