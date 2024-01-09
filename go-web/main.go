0f43efca6af1d3000d1b7af4a128b8e756fb2696
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2024-01-09 13:51:20 +0700

    update route code

--- go-web/main.go
+++ go-web/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
+
+    "github.com/gorilla/mux"
 )
 
 func main() {
-	// ex : http://localhost:80/
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
+    rt := mux.NewRouter()
 
-	fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
-	
-	// print : Server is running on port 80
-	fmt.Println("Server is running on port 80")
-	http.ListenAndServe(":80", nil)
+    rt.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["title"]
+        page := vars["page"]
+
+        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+    })
+
+    http.ListenAndServe(":80", rt)
 }
